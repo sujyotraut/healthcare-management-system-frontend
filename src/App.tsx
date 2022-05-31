b4e5cffaf2dcaf765a9588bcae49ab97c3a2011d
Sujyot <70472979+SujyotRaut@users.noreply.github.com>
2022-05-31 21:32:04 +0530

    Update MainNavbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
 import MainNavbar from './components/MainNavbar';
 import AddDoctorPage from './pages/AddDoctorPage';
-import AddMedicinePage from './pages/AddMedicinePage';
-import AddStaffPage from './pages/AddStaffPage';
-import AddWardPage from './pages/AddWardPage';
 import HomePage from './pages/HomePage';
 import ListDoctorsPage from './pages/ListDoctorsPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <>
       <MainNavbar />
-      <ListDoctorsPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='add-doctor' element={<AddDoctorPage />} />
+        <Route path='add-staff' element={<AddDoctorPage />} />
+      </Routes>
     </>
   );
 }
--- src/components/MainNavbar.tsx
+++ src/components/MainNavbar.tsx
@@ -1,49 +1,147 @@
 import React from 'react';
 import { Container, Nav, Navbar, Image, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const MainNavbar = () => {
-  return (
-    <Navbar bg='primary' variant='dark' expand='lg'>
-      <Container>
-        <Navbar.Brand href='#home'>Healthcare Management System</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='ms-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <NavDropdown title='Doctor'>
-              <NavDropdown.Item href='#'>Add Doctor</NavDropdown.Item>
-              <NavDropdown.Item href='#'>List Doctor</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title='Staff'>
-              <NavDropdown.Item href='#'>Add Staff</NavDropdown.Item>
-              <NavDropdown.Item href='#'>List Staff</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title='Ward'>
-              <NavDropdown.Item href='#'>Add Ward</NavDropdown.Item>
-              <NavDropdown.Item href='#'>List Ward</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title='Room'>
-              <NavDropdown.Item href='#'>Add Room</NavDropdown.Item>
-              <NavDropdown.Item href='#'>List Room</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title='Bed'>
-              <NavDropdown.Item href='#'>Add Bed</NavDropdown.Item>
-              <NavDropdown.Item href='#'>List Bed</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title='Medicine'>
-              <NavDropdown.Item href='#'>Add Medicine</NavDropdown.Item>
-              <NavDropdown.Item href='#'>List Medicine</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href='#'>Insurance</Nav.Link>
-            <NavDropdown title='Profile'>
-              <NavDropdown.Item href='#'>Add</NavDropdown.Item>
-              <NavDropdown.Item href='#'>List</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  const user: string = 'admin';
+  switch (user) {
+    case 'admin':
+      return (
+        <Navbar bg='primary' variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand>Healthcare Management System</Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='ms-auto'>
+                <Nav.Link as={Link} to='/'>
+                  Home
+                </Nav.Link>
+                <NavDropdown title='Doctor'>
+                  <NavDropdown.Item as={Link} to='add-doctor'>
+                    Add Doctor
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List Doctor</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title='Staff'>
+                  <NavDropdown.Item href='#'>Add Staff</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List Staff</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title='Ward'>
+                  <NavDropdown.Item href='#'>Add Ward</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List Ward</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title='Room'>
+                  <NavDropdown.Item href='#'>Add Room</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List Room</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title='Bed'>
+                  <NavDropdown.Item href='#'>Add Bed</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List Bed</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title='Medicine'>
+                  <NavDropdown.Item href='#'>Add Medicine</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List Medicine</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href='#'>Insurance</Nav.Link>
+                <NavDropdown title='Profile'>
+                  <NavDropdown.Item href='#'>Add</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+    case 'doctor':
+      return (
+        <Navbar bg='primary' variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand href='#home'>Healthcare Management System</Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='ms-auto'>
+                <Nav.Link href='#home'>Home</Nav.Link>
+                <NavDropdown title='Schedule Time'>
+                  <NavDropdown.Item href='#'>Add Schedule</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List Schedule</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href='#home'>Prescription</Nav.Link>
+                <Nav.Link href='#home'>Test Report</Nav.Link>
+                <NavDropdown title='Profile'>
+                  <NavDropdown.Item href='#'>Add</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+    case 'staff':
+      return (
+        <Navbar bg='primary' variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand href='#home'>Healthcare Management System</Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='ms-auto'>
+                <Nav.Link href='#home'>Home</Nav.Link>
+                <Nav.Link href='#home'>Doctor</Nav.Link>
+                <NavDropdown title='Patient'>
+                  <NavDropdown.Item href='#'>Admit Patient</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>Patient List</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title='Schedule Time'>
+                  <NavDropdown.Item href='#'>Add Schedule</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List Schedule</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title='Bill'>
+                  <NavDropdown.Item href='#'>Add Bill</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List Bill</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title='Test Report'>
+                  <NavDropdown.Item href='#'>Add Test Report</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List Test Report</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href='#'>Insurance</Nav.Link>
+                <NavDropdown title='Profile'>
+                  <NavDropdown.Item href='#'>Add</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+    default:
+      return (
+        <Navbar bg='primary' variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand href='#home'>Healthcare Management System</Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='ms-auto'>
+                <Nav.Link href='#home'>Home</Nav.Link>
+                <Nav.Link href='#'>Doctor</Nav.Link>
+                <Nav.Link href='#'>Doctor Timing</Nav.Link>
+                <Nav.Link href='#'>Appointments</Nav.Link>
+                <Nav.Link href='#'>Prescription</Nav.Link>
+                <Nav.Link href='#'>Medicine</Nav.Link>
+                <Nav.Link href='#'>Bills</Nav.Link>
+                <NavDropdown title='Insurance'>
+                  <NavDropdown.Item href='#'>Add Insurance</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List Insurance</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href='#'>Test Report</Nav.Link>
+                <NavDropdown title='Profile'>
+                  <NavDropdown.Item href='#'>Add</NavDropdown.Item>
+                  <NavDropdown.Item href='#'>List</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+  }
 };
 
 export default MainNavbar;
